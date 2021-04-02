0a6d55f24aa45d651ee9526c45702de6acfc6004
wangyongbo <wangyongbo@yeeyoon.com>
2021-04-02 12:52:51 +0800

    初始化

---
+++ src/app.config.js
@@ -0,0 +1,9 @@
+export default {
+  pages: ["pages/index/index"],
+  window: {
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "量尺宝",
+    navigationBarTextStyle: "black",
+  },
+};
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { View, Text } from "@tarojs/components";
+import "./index.scss";
+
+export default function Index(props) {
+  return (
+    <View className="index">
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
